6fb45b717b8ecd1df541b80afe3e1d7610b6ae2e
Szlacroxx <42041720+NoTreblee@users.noreply.github.com>
2026-02-25 18:39:37 +0100

    Fix variable expansion and improve aliases in setup-zsh.sh

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -81,7 +81,7 @@ fi
 echo "Configuring .zshrc..."
 cat > ~/.zshrc << EOF
 # Path to oh-my-zsh installation
-export ZSH="\$HOME/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set theme name (empty means "robbyrussell")
 ZSH_THEME=""
@@ -118,7 +118,7 @@ plugins=(
 )
 
 # Load Oh My Zsh
-source \$ZSH/oh-my-zsh.sh
+source $ZSH/oh-my-zsh.sh
 
 # ============================================
 # PLUGIN CONFIGURATION
@@ -179,7 +179,7 @@ ZSH_HIGHLIGHT_STYLES[arg0]='fg=cyan'
 # ============================================
 
 # Set ZSH as default shell
-export SHELL=${ZSH_PATH}
+export SHELL=/usr/bin/zsh
 
 # History
 HISTSIZE=100000
@@ -230,7 +230,7 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ~='cd ~'
 alias -- -='cd -'
-
+alias mkdir='mkdir -p'
 # Git
 alias gs='git status'
 alias ga='git add'
@@ -283,7 +283,7 @@ alias ln='ln -i'
 alias c='clear'
 alias h='history'
 alias j='jobs -l'
-alias path='echo -e \${PATH//:/\\n}'
+alias path='echo -e ${PATH//:/\n}'
 alias now='date +"%T"'
 alias nowdate='date +"%d-%m-%Y"'
 
@@ -304,22 +304,22 @@ fi
 
 # Create directory and go to it
 mkcd() {
-    mkdir -p "\$1" && cd "\$1"
+    mkdir -p "$1" && cd "$1"
 }
 
 # Find file
 ff() {
-    find . -type f -name "*\$1*"
+    find . -type f -name "*$1*"
 }
 
 # Find directory
 fd() {
-    find . -type d -name "*\$1*"
+    find . -type d -name "*$1*"
 }
 
 # Count lines in files
 countlines() {
-    find . -name "*.\$1" | xargs wc -l
+    find . -name "*.$1" | xargs wc -l
 }
 
 # Display public IP
@@ -330,14 +330,14 @@ myip() {
 
 # Weather
 weather() {
-    curl -s "wttr.in/\${1:-Warsaw}?0"
+    curl -s "wttr.in/${1:-Warsaw}?0"
 }
 
 # Encrypt file
 encrypt() {
-    if [ -f "\$1" ]; then
-        gpg -c "\$1"
-        echo "File \$1.gpg created"
+    if [ -f "$1" ]; then
+        gpg -c "$1"
+        echo "File $1.gpg created"
     else
         echo "Please provide a valid filename"
     fi
@@ -345,9 +345,9 @@ encrypt() {
 
 # Decrypt file
 decrypt() {
-    if [ -f "\$1" ]; then
-        gpg -d "\$1" > "\${1%.gpg}"
-        echo "File \${1%.gpg} decrypted"
+    if [ -f "$1" ]; then
+        gpg -d "$1" > "${1%.gpg}"
+        echo "File ${1%.gpg} decrypted"
     else
         echo "Please provide a valid filename"
     fi
@@ -358,34 +358,34 @@ decrypt() {
 # ============================================
 
 # Add directories to PATH
-export PATH="\$HOME/.local/bin:\$PATH"
-export PATH="\$HOME/bin:\$PATH"
-export PATH="/usr/local/bin:\$PATH"
-export PATH="/usr/local/sbin:\$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
 
 # Language
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 # Python
-export PYTHONSTARTUP="\$HOME/.pythonrc"
+export PYTHONSTARTUP="$HOME/.pythonrc"
 export PYTHONDONTWRITEBYTECODE=1
 
 # Node.js
-export NVM_DIR="\$HOME/.nvm"
-[ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"  # Load nvm
-[ -s "\$NVM_DIR/bash_completion" ] && \. "\$NVM_DIR/bash_completion"  # Load nvm completion
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Load nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # Load nvm completion
 
 # Go
-export GOPATH="\$HOME/go"
-export PATH="\$GOPATH/bin:\$PATH"
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin:$PATH"
 
 # Rust
-export PATH="\$HOME/.cargo/bin:\$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
 
 # Starship Prompt (if installed)
 if command -v starship &> /dev/null; then
-    eval "\$(starship init zsh)"
+    eval "$(starship init zsh)"
 fi
 
 # ============================================
@@ -417,17 +417,6 @@ else
     echo "Shell change skipped. You can change it later with: chsh -s $(which zsh)"
 fi
 
-echo ""
-echo ""
-echo ""
-echo " _      ____ _____ ____  _____ ____  _     _____ _____";
-echo "/ \\  /|/  _ Y__ __Y  __\\/  __//  _ \\/ \\   /  __//  __/";
-echo "| |\\ ||| / \\| / \\ |  \\/||  \\  | | //| |   |  \\  |  \\  ";
-echo "| | \\||| \\_/| | | |    /|  /_ | |_\\\\| |_/\\|  /_ |  /_ ";
-echo "\\_/  \\|\\____/ \\_/ \\_/\\_\\\\____\\\\____/\\____/\\____\\\\____\\";
-echo "                                                      ";
-echo ""
-echo ""
 echo "-------- Configuration completed successfully! -------"
 echo ""
 echo ""
